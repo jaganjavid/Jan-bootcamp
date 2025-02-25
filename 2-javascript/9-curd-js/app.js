169c574eeb69766d1956f2d51c8954f4ebae4885
Jagan <110019353+jaganjavid@users.noreply.github.com>
2025-02-25 19:58:48 +0530

    Added curd

--- 2-javascript/9-curd-js/app.js
+++ 2-javascript/9-curd-js/app.js
@@ -1,63 +1,131 @@
 
-const apiURL = "https://67bc778ded4861e07b3aa8a9.mockapi.io/users";
+const apiURL = "https://67bdc829321b883e790de61d.mockapi.io/users";
+let editID = null;
+let allPost = [];
 
+console.log("Hello")
+
+
+// Add post
+document.querySelector(".add-button").addEventListener("click", handleData);
+
+// Get Data
 document.addEventListener("DOMContentLoaded", fetchData);
-
-// Fetch and display data
 
 async function fetchData() {
     try{
         const response = await fetch(apiURL);
-        const data = await response.json();
-        renderData(data);
-    }catch(error){
-        console.error("Error in fetching data")
+        allPost = await response.json();
+        renderData();
+    }catch(err){
+        alert("Error");
     }
 }
 
+function showAlert(message, className){
 
-// Render data list
-function renderData(data){
-    
-    const dataList = document.querySelector("#data-list");
+    const divElement = document.createElement("div");
+    divElement.className = `alert ${className} fixed-top text-center`;
+    divElement.innerText = message;
 
-    dataList.innerHTML = "";
+    document.querySelector(".show-alert").appendChild(divElement);
 
-    data.forEach(data => {
-        
-        const listItem = document.createElement("li");
+    setTimeout(() => divElement.remove(), 3000);
 
-        listItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
-
-        listItem.innerText = data.name;
-
-        const buttonGroup = document.createElement("div");
-        buttonGroup.classList.add("btn-group");
-        
-        const editButton = createButton("Edit", "btn-warning", () => console.log(data.id));
-        const deleteButton = createButton("Delete", "btn-danger", () => console.log(data.id));
-
-        buttonGroup.append(editButton, deleteButton);
-
-        listItem.appendChild(buttonGroup);
-
-
-        dataList.appendChild(listItem);
-
-    })
 
 }
 
-// Create a button element
-function createButton(text, className, onClick){
 
-    const button = document.createElement("button");
+// Handle Data
+async function handleData() {
 
-    button.innerText = text;
-    button.classList.add("btn", className, "mx-2");
+    const inputData = document.querySelector("#inputData").value.trim();
 
-    button.addEventListener("click", onClick);
+   if(!inputData){
+    alert("Please fill the field");
+    return;
+   }
 
-    return button;
+   const method = editID ? "PUT" : "POST"
 
+   const url = editID ? `${apiURL}/${editID}` : apiURL;
+
+   const paylaod = {
+    name:inputData
+   }
+
+   try{
+    await fetch(url, {
+        method:method,
+        headers: {"Content-Type":"application/json"},
+        body:JSON.stringify(paylaod)
+    })
+
+    fetchData();
+    showAlert(editID ? "Post updated successfully" : "Post added successfully", "alert-success");
+    resetForm();
+   }catch(err){
+     console.log(err);
+   }
+    
+}
+
+async function deleteData(id){
+    try{
+        await fetch(`${apiURL}/${id}`, {method:"DELETE"});
+        console.log("Post deleted");
+        fetchData()
+    }catch(err){
+        console.log("error");
+    }
+}
+
+async function editData(id, name) {
+    document.querySelector("#inputData").value = name;
+    editID = id;
+
+    document.querySelector(".add-button").innerText = "Update Post";
+}
+
+
+function renderData(){
+
+    const dataList = document.querySelector("#dataList");
+    dataList.innerHTML = "";
+
+    
+
+    allPost.forEach(post => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `${post.name}`;
+
+        const div = document.createElement("div");
+
+       
+
+        const editButton = document.createElement("button");
+        editButton.innerText = "Edit";
+        editButton.classList.add("btn", "btn-warning", "mx-2");
+        editButton.addEventListener("click", () => editData(post.id, post.name));
+
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        deleteButton.classList.add("btn", "btn-danger");
+        deleteButton.addEventListener("click", () => deleteData(post.id));
+
+        div.appendChild(editButton)
+        div.appendChild(deleteButton)
+
+        listItem.appendChild(div);
+        dataList.append(listItem);
+    })
+
+    
+
+}
+
+function resetForm(){
+    editID = null;
+    document.querySelector("#inputData").value = "";
+    document.querySelector(".add-button").innerText = "Add Post";
 }
